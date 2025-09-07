07d16b029842517777d55515116ca7d81e8caab9
Tonnierich <richtonnie14@gmail.com>
2025-09-07 23:12:25 +0300

    Update colours.js

--- src/external/bot-skeleton/scratch/hooks/colours.js
+++ src/external/bot-skeleton/scratch/hooks/colours.js
@@ -1,40 +1,42 @@
+import Blockly from "blockly"
+
 const lightMode = () => {
-    const workspace = Blockly;
-    workspace.Colours.RootBlock = {
-        colour: '#4635B1',
-        colourSecondary: '#064e72',
-        colourTertiary: '#6d7278',
-    };
+  const workspace = Blockly
+  workspace.Colours.RootBlock = {
+    colour: "#4635B0",
+    colourSecondary: "#5645B6",
+    colourTertiary: "#A189F6",
+  }
 
-    workspace.Colours.Base = {
-        colour: '#e5e5e5',
-        colourSecondary: '#ffffff',
-        colourTertiary: '#6d7278',
-    };
+  workspace.Colours.Base = {
+    colour: "#E4E5E5",
+    colourSecondary: "#ffffff",
+    colourTertiary: "#A189F6",
+  }
 
-    workspace.Colours.Special1 = {
-        colour: '#e5e5e5',
-        colourSecondary: '#ffffff',
-        colourTertiary: '#6d7278',
-    };
+  workspace.Colours.Special1 = {
+    colour: "#5645B6",
+    colourSecondary: "#4635B0",
+    colourTertiary: "#A189F6",
+  }
 
-    workspace.Colours.Special2 = {
-        colour: '#e5e5e5',
-        colourSecondary: '#ffffff',
-        colourTertiary: '#6d7278',
-    };
+  workspace.Colours.Special2 = {
+    colour: "#4635B0",
+    colourSecondary: "#5645B6",
+    colourTertiary: "#A189F6",
+  }
 
-    workspace.Colours.Special3 = {
-        colour: '#e5e5e5',
-        colourSecondary: '#ffffff',
-        colourTertiary: '#6d7278',
-    };
+  workspace.Colours.Special3 = {
+    colour: "#A189F6",
+    colourSecondary: "#E4E5E5",
+    colourTertiary: "#5645B6",
+  }
 
-    workspace.Colours.Special4 = {
-        colour: '#e5e5e5',
-        colourSecondary: '#000000',
-        colourTertiary: '#0e0e0e',
-    };
-};
+  workspace.Colours.Special4 = {
+    colour: "#161415",
+    colourSecondary: "#F1F4F5",
+    colourTertiary: "#4635B0",
+  }
+}
 
-export const setColors = () => lightMode();
+export const setColors = () => lightMode()
